0d3f7e797bf0929f5b008b5bab9b9b6d4ff74920
momen hesham <hmomen235@gmail.com>
2024-03-14 16:59:00 +0200

    [add]

--- js/main.js
+++ js/main.js
@@ -1,21 +1,11 @@
-// const webCamElement = document.getElementById("webcam");
-// const canvasElement = document.getElementById("canvas");
-
-// const webcam = new Webcam(webCamElement, "user", canvasElement);
-
-// webcam.start();
-
-// function takePicture() {
-//   let picture = webcam.snap();
-//   document.querySelector("a").href = picture;
-// }
+"use strict";
 
 const webCamElement = document.getElementById("webcam");
 const canvasElement = document.getElementById("canvas");
 
 const constraints = {
   video: true,
-  facingMode: "environment",
+  facingMode: "externally",
 };
 
 const webcam = new Webcam(webCamElement, constraints, canvasElement);

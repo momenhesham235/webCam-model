2fa7e0ca6fa2d379e77350176b109542f6ef6e29
momen hesham <hmomen235@gmail.com>
2024-03-15 23:07:01 +0200

    [add] local webcam

--- js/main.js
+++ js/main.js
@@ -6,14 +6,50 @@ const canvasElement = document.getElementById("canvas");
 const constraints = {
   audio: false,
   video: true,
+  image: {
+    width: {
+      min: 320,
+      ideal: 640,
+      max: 640,
+    },
+    height: {
+      min: 240,
+      ideal: 480,
+      max: 480,
+    },
+    aspectRatio: {
+      ideal: 1.7777777777777777,
+    },
+    frameRate: {
+      min: 15,
+      ideal: 30,
+      max: 30,
+    },
+    imageQuality: {
+      min: 0,
+      ideal: 1,
+      max: 1,
+    },
+    noiseReduction: {
+      min: 0,
+      ideal: 1,
+      max: 1,
+    },
+    resizeQuality: {
+      min: 0,
+      ideal: 1,
+      max: 1,
+    },
+    imageSmoothing: {
+      min: 0,
+      ideal: 1,
+      max: 1,
+    },
+  },
   facingMode: "environment",
 };
 
 const webcam = new Webcam(webCamElement, constraints, canvasElement);
-
-webcam.onSuccess = () => {
-  console.log("webcam on success");
-};
 
 function cameraFlip() {
   console.log("flip");
@@ -24,5 +60,6 @@ function cameraFlip() {
 webcam.start();
 function takePicture() {
   let picture = webcam.snap();
+
   document.querySelector("a").href = picture;
 }
